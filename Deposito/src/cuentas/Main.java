99003583005970c29e4dc29fda6a5d004e390b93
edivieito <edivieito@192.168.0.19>
2022-02-20 20:39:42 +0100

    Introducir el método operativa_cuenta

--- Deposito/src/cuentas/Main.java
+++ Deposito/src/cuentas/Main.java
@@ -2,23 +2,27 @@ package cuentas;
 public class Main {
 
     public static void main(String[] args) {
-        CCuenta cuenta1;
+        operativa_cuenta();
+    }
+
+	private static void operativa_cuenta() {
+		CCuenta miCuenta;
         double saldoActual;
 
-        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
-        saldoActual = cuenta1.estado();
+        miCuenta = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        saldoActual = miCuenta.estado();
         System.out.println("El saldo actual es"+ saldoActual );
 
         try {
-            cuenta1.retirar(2300);
+            miCuenta.retirar(2300);
         } catch (Exception e) {
             System.out.print("Fallo al retirar");
         }
         try {
             System.out.println("Ingreso en cuenta");
-            cuenta1.ingresar(695);
+            miCuenta.ingresar(695);
         } catch (Exception e) {
             System.out.print("Fallo al ingresar");
         }
-    }
+	}
 }
