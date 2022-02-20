963d1ec274bc0c868964e723caa3d39d2a67da62
edivieito <edivieito@192.168.0.19>
2022-02-20 20:43:06 +0100

    Encapsular atributos

--- Deposito/src/cuentas/CCuenta.java
+++ Deposito/src/cuentas/CCuenta.java
@@ -13,21 +13,21 @@ public class CCuenta {
 
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
-        nombre =nom;
-        cuenta=cue;
-        saldo=sal;
+        setNombre(nom);
+        setCuenta(cue);
+        setSaldo(sal);
     }
 
     public double estado()
     {
-        return saldo;
+        return getSaldo();
     }
 
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
-        saldo = saldo + cantidad;
+        setSaldo(getSaldo() + cantidad);
     }
 
     public void retirar(double cantidad) throws Exception
@@ -36,6 +36,38 @@ public class CCuenta {
             throw new Exception ("No se puede retirar una cantidad negativa");
         if (estado()< cantidad)
             throw new Exception ("No se hay suficiente saldo");
-        saldo = saldo - cantidad;
+        setSaldo(getSaldo() - cantidad);
     }
+
+	private String getNombre() {
+		return nombre;
+	}
+
+	private void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	private String getCuenta() {
+		return cuenta;
+	}
+
+	private void setCuenta(String cuenta) {
+		this.cuenta = cuenta;
+	}
+
+	private double getSaldo() {
+		return saldo;
+	}
+
+	private void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	private double getTipoInterés() {
+		return tipoInterés;
+	}
+
+	private void setTipoInterés(double tipoInterés) {
+		this.tipoInterés = tipoInterés;
+	}
 }
